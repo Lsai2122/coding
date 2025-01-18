c0c312feb5d0bfe2dd3ba5b69d5a8ede80b540dd
Lsai2122 <leelasaineralla@gmail.com>
2025-01-18 11:57:04 +0530

    g

--- c files/DS folders/quicksort.c
+++ c files/DS folders/quicksort.c
@@ -1,43 +1,51 @@
-#include<stdio.h>
-void quick_sort(int*arr,int l,int h)
+#include <stdio.h>
+void quick_sort(int arr[], int l, int h)
 {
-    int ei = h,si = l,pivot = arr[l];
-    while(ei<si)
+    int ei = h, si = l, pivot = arr[l];
+    while (ei > si)
     {
-        while(arr[si]<pivot)
+        while (arr[si] <= pivot && si < h)
         {
             si++;
         }
-        while(arr[ei]>pivot)
+        while (arr[ei] >= pivot && ei > l)
         {
-            ei++;
+            ei--;
         }
-        int temp = arr[si];
-        arr[si] = arr[ei];
-        arr[ei] = temp;
+        if (ei > si)
+        {
+            int temp = arr[si];
+            arr[si] = arr[ei];
+            arr[ei] = temp;
+        }
     }
-    arr[l]=arr[ei];
-    arr[ei]= pivot;
-    if(l<h){
+    int temp = arr[l];
+    arr[l] = arr[ei];
+    arr[ei] = temp;
+    if(ei>l)
+    {
         quick_sort(arr,l,ei-1);
+    }
+    else if (ei<h)
+    {
         quick_sort(arr,ei+1,h);
     }
-    
+    return;
 }
 int main()
 {
     int n;
     printf("Enter number of Elements : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("enter element %d: ",i+1);
-        scanf("%d",arr[i]);
+        printf("enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
     }
-    quick_sort(arr,0,n-1);
-    for(int i=0;i<n;i++)
+    quick_sort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++)
     {
-        printf("%d ",arr[i]);
+        printf("%d ", arr[i]);
     }
 }
---
+++ c files/DS folders/test.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        
+        for(int j =i+1;arr[j-1]>arr[j]&&j>0;j--)
+        {
+            int temp= arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d, ",arr[i]);
+    }
+}
