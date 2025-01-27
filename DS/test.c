89a26f3d4a2de31c35062854ac6ff3f405fa7e11
Lsai2122 <leelasaineralla@gmail.com>
2025-01-27 18:34:25 +0530

    27-1-25

--- DS/test.c
+++ DS/test.c
@@ -1,14 +1,39 @@
+#include<string.h>
 #include<stdio.h>
-struct a
+int subarray(char* s,int l,int h,int max)
 {
-    char i[10];
-    int j;
-} b[2]={
-    {"aai",1},
-    {"ajay",2}
-};
-int main()
-{
-    char * m[2]={b[0].i,b[1].i};
-    printf("%d",*(m[0]+1)<*(m[1]+1));
+    if(l<h)
+    {
+        max = subarray(s,l+1,h,max);
+    }
+    int a=1;
+    for(int i=l;i<=h-1;i++)
+    {
+        for(int j =i+1;j<=h;j++)
+        {
+            if(a==0)
+            {
+                break;
+            }
+            if(s[i]==s[j])
+            {
+                a=0;
+            }
+
+        }
+        if(a==0)
+        {
+            break;
+        }
+    }
+    if(a==1&&max<h-l+1)
+    {
+        return h-l+1;
+    }
+    return max;
+}
+int lengthOfLongestSubstring(char* s) {
+    int size = strlen(s);
+    int max = subarray(s,0,size-1,0);
+    return max;
 }
